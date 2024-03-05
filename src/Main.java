3b72c44d0ef690b3082372c9c6187ecc9e0c1b17
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2024-03-05 16:03:26 +0000

    Added basic JDBC connection and transaction handling

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,91 @@
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        // JDBC related variables
+        String ip = "localhost";
+        String port = "3306";
+        String database = "projeto";
+        String parameters  = "?useTimezone=true&serverTimezone=UTC&verifyServerCertificate=false&useSSL=true";
+        String url = "jdbc:mysql://" + ip + ":" + port + "/" + database + parameters;
+        String user = "root";
+        String password = "toor";
+
+        // JDBC objects
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        // Load the JDBC driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            System.out.println("Exception: " + e);
+            System.exit(1);
+        }
+
+        // Connect to the database
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            st = conn.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Connection failed.");
+            System.out.println("Exception: " + e);
+            System.exit(1);
+        }
+
+        // Transaction control
+        try {
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            System.out.println("Failed to turn off auto-commit.");
+            System.out.println("Exception: " + e);
+            System.exit(1);
+        }
+
+        // Open scanner for user input
         Scanner scanner = new Scanner(System.in);
-        doUsersExist(scanner);
-        mainLoop(scanner);
+
+        // Application and SQL handling
+        try {
+            doUsersExist(scanner);
+            mainLoop(scanner);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                System.out.println("Failed to rollback transaction.");
+                System.out.println("Exception: " + e2);
+            }
+        } finally {
+            if (st != null) {
+                try {
+                    st.close();
+                } catch (SQLException e) {
+                    System.out.println("Failed to close statement.");
+                    System.out.println("Exception: " + e);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    System.out.println("Failed to close connection.");
+                    System.out.println("Exception: " + e);
+                }
+            }
+        }
+
+        // Close scanner and exit application
         scanner.close();
+        System.exit(0);
     }
 
     private static void doUsersExist(Scanner scanner) {
